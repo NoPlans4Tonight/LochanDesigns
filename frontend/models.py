2432d3c7f8cfb73f86cee37e31fb28e6942dd3a5
NoPlans4Tonight <jlochan53@gmail.com>
2022-10-18 21:51:39 -0500

    photo upload working

--- frontend/models.py
+++ frontend/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(null=True, blank=True, upload_to='images/')
+    image = models.ImageField(null=True, blank=True, upload_to='media/')
 
     def __str__(self):
         return f'{self.product_name}'
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
     path('products/', views.products, name='products'),
     # path("", views.inventory, name='inventory')
     path("", views.home_page)
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)

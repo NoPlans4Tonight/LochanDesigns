76f8cc7036c0a94c3b107df52bbd059a8656285d
NoPlans4Tonight <61126433+NoPlans4Tonight@users.noreply.github.com>
2022-10-18 21:59:51 -0500

    Photo backend working (#2)
    * Updates
    
    * Product Page begin. Modulize navbar and page body
    
    * photo upload working

--- frontend/models.py
+++ frontend/models.py
@@ -6,7 +6,11 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
+<<<<<<< HEAD
+    image = models.ImageField(null=True, blank=True, upload_to='media/')
+=======
     image = models.ImageField(null=True, blank=True, upload_to='images/')
+>>>>>>> main
 
     def __str__(self):
         return f'{self.product_name}'
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
     path('products/', views.products, name='products'),
     # path("", views.inventory, name='inventory')
     path("", views.home_page)
-
+<<<<<<< HEAD
 ]
+=======
+>>>>>>> main
+
+urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)

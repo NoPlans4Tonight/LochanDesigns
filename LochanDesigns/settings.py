e68b1c72bb8002aa46db4ed7664305e07ac03c15
NoPlans4Tonight <jlochan53@gmail.com>
2022-11-24 12:50:00 -0600

    remove test gallery images

--- LochanDesigns/settings.py
+++ LochanDesigns/settings.py
@@ -140,7 +140,8 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER = 'haley@lochandesigns.com'

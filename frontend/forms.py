bb1d0bb861f672a4592b45547d2b9af5a40f0108
NoPlans4Tonight <jlochan53@gmail.com>
2022-12-01 16:14:51 -0600

    validation handling & bot catching

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.core import validators
 
 class ContactForm(forms.Form):
 	first_name = forms.CharField(max_length = 50)
 	last_name = forms.CharField(max_length = 50)
 	email_address = forms.EmailField(max_length = 150)
 	message = forms.CharField(widget = forms.Textarea, max_length = 2000)
+	botcatcher = forms.CharField(required=False,
+								widget=forms.HiddenInput,
+								validators=[validators.MaxLengthValidator(0)])
+	
--- frontend/views.py
+++ frontend/views.py
@@ -18,18 +18,19 @@ def contact_page(request):
 		if form.is_valid():
 			subject = "Website Inquiry" 
 			body = {
-			'first_name': form.cleaned_data['first_name'], 
-			'last_name': form.cleaned_data['last_name'], 
-			'email': form.cleaned_data['email_address'], 
-			'message':form.cleaned_data['message'], 
+			"First Name: " + form.cleaned_data['first_name'], 
+			"Last Name: " + form.cleaned_data['last_name'], 
+			"Email: " + form.cleaned_data['email_address'], 
+			"Message: " + form.cleaned_data['message'], 
 			}
-			message = "\n".join(body.values())
-
+			message = "\n".join(body)
 			try:
 				send_mail(subject, message, 'haley@lochandesigns.com', ['haley@lochandesigns.com', 'jlochan53@gmail.com'])
 			except BadHeaderError:
 				return HttpResponse('Invalid header found.')
 			return render(request, "contact.html",{'form':form, 'successful_submit': True})
+		else:
+			return render(request, "contact.html", {'form':form})
 
 	form = ContactForm()
 	return render(request, "contact.html", {'form':form})

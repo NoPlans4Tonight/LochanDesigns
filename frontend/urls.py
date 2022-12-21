fac601c70aa44b47b8d56208406752f5c0a4b366
NoPlans4Tonight <jlochan53@gmail.com>
2022-12-21 14:31:55 -0600

    created user login,register, and logout auth

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,7 +5,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('contact/', views.contact_page, name='contact'),
-    path('', views.home_page, name='home')
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_page, name="register"),
+    path('', views.home_page, name='home'),
 ] 
 urlpatterns  +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

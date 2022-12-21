a819c3c713f665e1b025a081dc7f6560504d7088
NoPlans4Tonight <jlochan53@gmail.com>
2022-12-21 11:19:47 -0600

    debug status

--- LochanDesigns/settings.py
+++ LochanDesigns/settings.py
@@ -65,9 +65,11 @@ MIDDLEWARE = [
 if (os.environ.get("APP_ENV") == "local"):
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = False
+    DEBUG = True
 else:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
+    DEBUG = False
 
 ROOT_URLCONF = 'LochanDesigns.urls'
 

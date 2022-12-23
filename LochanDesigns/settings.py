aa4b7fbe89b81e8ed13f460656e1cb1d796c92d0
NoPlans4Tonight <jlochan53@gmail.com>
2022-12-22 21:50:38 -0600

    heroku db on prod server

--- LochanDesigns/settings.py
+++ LochanDesigns/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,6 +92,7 @@ WSGI_APPLICATION = 'LochanDesigns.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+MAX_CONN_AGE = 600
 
 DATABASES = {
     'default': {
@@ -100,7 +101,16 @@ DATABASES = {
     }
 }
 
-
+if (os.environ.get("APP_ENV") == "local"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 

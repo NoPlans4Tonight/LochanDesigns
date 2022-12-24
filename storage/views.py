b4a0b2625c074a03b7f49bbca91ab35950205c49
Jonathon Lochan <61126433+NoPlans4Tonight@users.noreply.github.com>
2022-12-23 21:47:23 -0600

    upload photo form created (#13)

--- storage/views.py
+++ storage/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from .forms import GalleryUploadForm
 from .forms import GalleryUploadForm
 from django.core.files import File
 from .forms import GalleryUploadForm
@@ -21,6 +23,21 @@ def products(request):
 
 	return render(request, 'products.html', {'page_obj': products})
 
+def edit_products(request):
+	return render(request, 'maintenance/maintenance.html')
+
+def upload_product(request):
+	if request.method == 'POST':
+		form = GalleryUploadForm(request.POST, request.FILES)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponse('successfully uploaded')
+	else:
+		form = GalleryUploadForm()
+
+	form = GalleryUploadForm()
+	return render(request, 'maintenance/upload.html', {"form": form})
 def edit_products(request):
 	return render(request, 'maintenance/maintenance.html')
 

962a7b3707ddcf2ef0e3f6786b14d23b275e9146
NoPlans4Tonight <jlochan53@gmail.com>
2023-05-24 10:22:07 -0500

    update contact form to use crispy form

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from django.forms import ClearableFileInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core import validators
@@ -11,6 +14,20 @@ class ContactForm(forms.Form):
 	botcatcher = forms.CharField(required=False,
 								widget=forms.HiddenInput,
 								validators=[validators.MaxLengthValidator(0)])
+	def __init__(self, *args, **kwargs):
+		super(ContactForm, self).__init__(*args, **kwargs)
+		self.helper = FormHelper()
+		self.helper.form_class = 'form-horizontal'
+		self.helper.label_class = 'label label-default text-light'
+		self.helper.field_class = 'form-control'
+		self.helper.layout = Layout(
+			'first_name',
+			'last_name',
+			'email_address',
+			'message',
+			'botcatcher',
+			Submit('submit', 'Submit', css_class='btn btn-secondary')
+		)
 
 class CreateUserForm(UserCreationForm):
     class Meta:
